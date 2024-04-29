ccbc8ed5ec50077f99a0553ff60ff865e5f4012e
Hugo Madge Leon <hugo.madgeleon@hest.ethz.ch>
2024-04-29 18:28:55 +0200

    update function locations

--- code/part1/lstm.py
+++ code/part1/lstm.py
@@ -1,11 +1,10 @@
 """ LSTM models for Q3 in Part 1 """
 from pathlib import Path
 
-import tensorflow as tf
-from sklearn.metrics import auc, precision_recall_curve, roc_auc_score
 from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Dropout
+from tensorflow.keras.metrics import AUC, Precision, Recall
 from tensorflow.keras.models import Sequential
-from utils import load_train_test, reshape_data
+from utils import fit_evaluate, load_train_test, reshape_data
 
 
 # Define the LSTM models
@@ -21,9 +20,9 @@ def lstm_model(X_train):
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
                   metrics=[
-                      tf.keras.metrics.AUC(name='auc'),
-                      tf.keras.metrics.Precision(name='precision'),
-                      tf.keras.metrics.Recall(name='recall')
+                      AUC(name='auc'),
+                      Precision(name='precision'),
+                      Recall(name='recall')
                   ])
     return model
 
@@ -40,27 +39,11 @@ def lstm_model_bidirectional(X_train):
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
                   metrics=[
-                      tf.keras.metrics.AUC(name='auc'),
-                      tf.keras.metrics.Precision(name='precision'),
-                      tf.keras.metrics.Recall(name='recall')
+                      AUC(name='auc'),
+                      Precision(name='precision'),
+                      Recall(name='recall')
                   ])
     return model
-
-
-# Fit and evaluate models
-def fit_evaluate(model, X_train, y_train, X_test, y_test):
-    _ = model.fit(X_train, y_train,
-                  epochs=50, batch_size=64,
-                  validation_split=0.1)
-
-    predictions = model.predict(X_test)
-
-    roc_score = roc_auc_score(y_test, predictions)
-    print(f"ROC-AUC: {roc_score:.3f}")
-
-    precision, recall, _ = precision_recall_curve(y_test, predictions)
-    auprc_score = auc(recall, precision)
-    print(f"AUPRC: {auprc_score:.3f} \n")
 
 
 if __name__ == "__main__":
--- code/part1/utils.py
+++ code/part1/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import auc, precision_recall_curve, roc_auc_score
 
 
 # TODO: Add headers to the data
@@ -20,3 +21,21 @@ def load_train_test(dpath="../../data/"):
 # Reshape the data for LSTM
 def reshape_data(X):
     return X.values.reshape((X.shape[0], X.shape[1], 1))
+
+
+# Fit and evaluate models
+def fit_evaluate(model, X_train, y_train, X_test, y_test,
+                 epochs=50, batch_size=64, val_split=0.1):
+
+    _ = model.fit(X_train, y_train,
+                  epochs=epochs, batch_size=batch_size,
+                  validation_split=val_split)
+
+    predictions = model.predict(X_test)
+
+    roc_score = roc_auc_score(y_test, predictions)
+    print(f"ROC-AUC: {roc_score:.3f}")
+
+    precision, recall, _ = precision_recall_curve(y_test, predictions)
+    auprc_score = auc(recall, precision)
+    print(f"AUPRC: {auprc_score:.3f} \n")
